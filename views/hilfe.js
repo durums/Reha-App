4ba069c15b29e9278893f6d1c4497ae707161fc0
kebabti0 <yusuf080206@hotmail.com>
2025-11-09 10:36:25 +0100

    hilfe.js
    pfad angepasst

--- views/hilfe.js
+++ views/hilfe.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Sicherstellen, dass feedback.js geladen ist
   const script = document.createElement('script');
-  script.src = '/js/feedback.js'; // <== PFAD ANPASSEN!
+  script.src = '/views/feedback.js'; // <== PFAD ANPASSEN!
   script.defer = true;
   document.head.appendChild(script);
 });
