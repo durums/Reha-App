1873c3644b134cd5a27bd11e37132a21bd140f29
Mia <mramis@stud.macromedia.de>
2025-11-20 16:46:38 +0100

    Delete pdf.js script and worker setup
    Remove pdf.js global script inclusion.

--- views/pdf.js
+++ views/pdf.js
@@ -1,7 +1,1 @@
-<!-- pdf.js global einbinden -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.min.js"></script>
-<script>
-  // Worker für pdf.js setzen
-  pdfjsLib.GlobalWorkerOptions.workerSrc =
-    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.js";
-</script>
+
